c99c8708c59df110c8f1b382e269f18bc5e9cb1d
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-25 16:06:17 +0100

    new types

--- src/features/travel-times/types.ts
+++ src/features/travel-times/types.ts
@@ -4,10 +4,151 @@ export type Location = {
   name: string;
 };
 
+export type PublicTransportTrip = {
+  departureTime: string;
+  arrivalTime: string;
+  duration: string;
+};
+
+export type Coordinates = {
+  lat: string;
+  lng: string;
+};
+
 export type Trip = {
   id: number;
   from: Location;
   to: Location;
   publicTransitTime: number;
   carTime: number;
+};
+
+export type Leg = {
+  Origin: Origin;
+  Destination: Origin;
+  Notes: {
+    Note: [
+      {
+        value: string;
+        key: string;
+        type: string;
+        routeIdxFrom: number;
+        routeIdxTo: number;
+        txtN: string;
+      }
+    ];
+  };
+  JourneyDetailRef: {
+    ref: string;
+  };
+  JourneyStatus: string;
+  Product: [
+    {
+      icon: {
+        res: string;
+      };
+      operatorInfo: {
+        name: string;
+        nameS: string;
+        nameN: string;
+        nameL: string;
+        id: string;
+      };
+      name: string;
+      internalName: string;
+      displayNumber: string;
+      num: string;
+      line: string;
+      lineId: string;
+      catOut: string;
+      catIn: string;
+      catCode: string;
+      cls: string;
+      catOutS: string;
+      catOutL: string;
+      operatorCode: string;
+      operator: string;
+      admin: string;
+      routeIdxFrom: number;
+      routeIdxTo: number;
+      matchId: string;
+    }
+  ];
+  Stops: {
+    Stop: Stop[];
+  };
+  JourneyDetail: {
+    ref: string;
+    dayOfOperation: string;
+  };
+  id: string;
+  idx: number;
+  name: string;
+  number: string;
+  category: string;
+  type: string;
+  reachable: boolean;
+  waitingState: string;
+  direction: string;
+  directionFlag: string;
+  duration: string;
+};
+export type Stop = {
+  name: string;
+  id: string;
+  extId: string;
+  routeIdx: number;
+  lon: number;
+  lat: number;
+  arrTime: string;
+  arrDate: string;
+  minimumChangeDuration: string;
+};
+
+export type Walk = {
+  Origin: Origin;
+  Destination: Destination;
+  GisRef: GisRef;
+  GisRoute: GisRoute;
+  Product: Product;
+  id: string;
+  idx: number;
+  name: string;
+  type: string;
+  duration: string;
+  dist: number;
+};
+
+type GisRef = {
+  ref: string;
+};
+type GisRoute = {
+  dist: number;
+  durS: string;
+  dirGeo: number;
+};
+type Destination = {
+  name: string;
+  type: string;
+  id: string;
+  extId: string;
+  lon: number;
+  lat: number;
+  time: string;
+  date: string;
+  minimumChangeDuration: string;
+};
+type Origin = {
+  name: string;
+  type: string;
+  id: string;
+  lon: number;
+  lat: number;
+  time: string;
+  date: string;
+};
+type Product = {
+  icon: string[];
+  name: string;
+  internalName: string;
 };

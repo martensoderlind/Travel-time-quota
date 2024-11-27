e5addaf10036876b34dbed4a83f82a41de413074
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-27 15:12:06 +0100

    marker works again

--- src/features/travel-times/components/travel-map.tsx
+++ src/features/travel-times/components/travel-map.tsx
@@ -1,10 +1,15 @@
-import dynamic from "next/dynamic";
 import { MapContainer, Polyline, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression } from "leaflet";
+import L, { LatLngExpression } from "leaflet";
 import { ClickableMarker } from "./clickable-marker";
 import { MapClickHandler } from "./map-click-handler";
-import { useEffect } from "react";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 type Props = {
   setFrom: React.Dispatch<React.SetStateAction<string>>;
